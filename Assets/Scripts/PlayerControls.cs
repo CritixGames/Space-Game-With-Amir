b2fd22d1ec547f45de452fc887e6833095ee752c
Cristian Gasparesc (CritixGames) <93558364+CritixGames@users.noreply.github.com>
2023-03-23 22:59:54 -0400

    MANY THINGS CHANGED

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    //Input Variables
+    float xThrow;
+    float yThrow;
+
     [SerializeField] float controlSpeed = 10f; //movement speed
 
     [SerializeField] float xRange = 5f; //clamp x range
     [SerializeField] float yRange = 5f; //clamp x range
 
+    //Pitch Variables
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    //Yaw Variables
+    [SerializeField] float positionYawFactor = -5f;
+    //Roll Variables
+    [SerializeField] float controlRollFactor = -20;
+
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
         //Movement OLD INPUT SYSTEM
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         //offset of movement = speed variable * input * Frame independent code
-        float xoffset = controlSpeed*xThrow*Time.deltaTime; 
+        float xoffset = controlSpeed * xThrow * Time.deltaTime;
         float rawXPOS = transform.localPosition.x + xoffset;//new x position = old one + offset
 
         //clamp to make sure player doesn't go off screen (X AXIS)
-        float clampedXPos = Mathf.Clamp(rawXPOS,-xRange,xRange);
+        float clampedXPos = Mathf.Clamp(rawXPOS, -xRange, xRange);
 
         //offset of movement = speed variable * input * Frame independent code
         float yoffset = controlSpeed * yThrow * Time.deltaTime;
@@ -31,10 +49,21 @@ public class PlayerControls : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPOS, -yRange, yRange);
 
         //finding player ship starting position and/or setting to new position
-        transform.localPosition = 
+        transform.localPosition =
            new Vector3(
            clampedXPos,
            clampedYPos,
            transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;  //Pitch Rotation
+        float yaw = transform.localPosition.x * positionYawFactor; //Yaw Rotation
+        float roll = xThrow * controlRollFactor; //Roll Rotation
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
